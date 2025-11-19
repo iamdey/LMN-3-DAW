502fdfd17b686f99761225dc5902dee31a73153d
David Epely <65737+iamdey@users.noreply.github.com>
2025-11-19 22:15:50 +0100

    display velocity on step sequencer as different height

--- Source/Views/Edit/Sequencers/StepSequencer/StepSequencerGridComponent.cpp
+++ Source/Views/Edit/Sequencers/StepSequencer/StepSequencerGridComponent.cpp
@@ -47,13 +47,14 @@ void StepSequencerGridComponent::paint(juce::Graphics &g) {
              noteIndex++) {
             int velocity = viewModel.hasNoteAt(channelNumber, noteIndex);
             if (velocity > 0) {
+                float heightDiff = (7 - velocity) * rowSpacing / 10;
                 if (noteIndex < viewModel.getNumberOfNotes()) {
                     g.setColour(appLookAndFeel.yellowColour);
-                    g.fillRect(noteX, channelY, colSpacing, rowSpacing);
+                    g.fillRect(noteX, channelY + heightDiff, colSpacing, rowSpacing - heightDiff);
 
                 } else {
                     g.setColour(appLookAndFeel.colour3.withAlpha(.3f));
-                    g.fillRect(noteX, channelY, colSpacing, rowSpacing);
+                    g.fillRect(noteX, channelY + heightDiff, colSpacing, rowSpacing - heightDiff);
                 }
             }
 
